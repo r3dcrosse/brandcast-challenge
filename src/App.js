59c9e638fac408243eb6eb9509ce4e8e5ffac971
David Wayman <mrdavidwayman@gmail.com>
2017-04-10 15:15:24 -0700

    Adds slider and slider state in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sliderValue: 250
+    }
+
+    this.handleSliderChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      sliderValue: 250
+    });
+  }
+
+  handleSliderChange(sliderValue) {
+    console.log(sliderValue);
+    if (sliderValue !== undefined) {
+      this.setState({
+        sliderValue: sliderValue
+      });
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Slider min={10} max={400} defaultValue={250} onChange={(val) => this.handleSliderChange(val)}/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
